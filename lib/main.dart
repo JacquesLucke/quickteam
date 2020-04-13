6e71f5b2bf239090447d71948497246750f2824e
Jacques Lucke <mail@jlucke.com>
2020-04-13 13:26:56 +0200

    initial functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(QuickTeamApp());
@@ -21,10 +23,101 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<PersonData> _persons;
+  int _nextIdentifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _persons = [];
+    _nextIdentifier = 1;
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    setState(() {
+      _persons.add(PersonData(
+          details.localPosition, _nextIdentifier.toString(), Colors.blue));
+      _nextIdentifier++;
+    });
+  }
+
+  void _makeTeams() {
+    setState(() {
+      var rng = Random();
+      for (PersonData person in _persons) {
+        person.color = Color.fromARGB(
+            255, rng.nextInt(255), rng.nextInt(255), rng.nextInt(255));
+      }
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _persons.clear();
+      _nextIdentifier = 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> widgets = [];
+    for (PersonData person in _persons) {
+      widgets.add(Positioned(
+        child: FloatingActionButton(
+          child: Text(person.identifier),
+          onPressed: () {
+            setState(() {
+              _persons.remove(person);
+            });
+          },
+          backgroundColor: person.color,
+        ),
+        left: person.position.dx,
+        top: person.position.dy,
+      ));
+    }
+
     return Scaffold(
-      body: Center(child: Text('Hello World')),
+      appBar: AppBar(
+        title: Center(
+          child: Row(
+            children: [
+              RaisedButton(
+                child: Text('Make Teams'),
+                onPressed: _makeTeams,
+              ),
+              RaisedButton(
+                child: Text('Reset'),
+                onPressed: _reset,
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              onTapDown: _onTapDown,
+              child: Container(
+                constraints: BoxConstraints.expand(),
+                color: Colors.red,
+                child: Stack(
+                  children: widgets,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class PersonData {
+  Offset position;
+  String identifier;
+  Color color;
+
+  PersonData(this.position, this.identifier, this.color);
 }
